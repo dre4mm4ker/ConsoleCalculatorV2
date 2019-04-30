5ca1d7033e2797475a24d34fdbce9cc31b226c21
Timur <dre4mm4ker@gmail.com>
2019-04-30 13:14:42 +0300

    upd

--- src/testApp/Main.java
+++ src/testApp/Main.java
@@ -1,17 +1,21 @@
 package testApp;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
-        try {
-            Splitter ch = new Splitter();
-            Maths maths = new Maths(ch.sign, ch.ar, ch.a, ch.b);
-            System.out.println(maths.result);
-        }
-        catch (InputException e) {
-            e.printStackTrace();
-            System.exit(0);
+        while (true) {
+            try {
+                Scanner sc = new Scanner(System.in);
+                String s = sc.nextLine();
+                Splitter ch = new Splitter(s);
+                Maths maths = new Maths(ch.sign, ch.ar, ch.a, ch.b);
+                System.out.println(maths.result);
+            } catch (InputException e) {
+                e.printStackTrace();
+                System.exit(0);
+            }
         }
     }
 }
--- src/testApp/Splitter.java
+++ src/testApp/Splitter.java
@@ -1,6 +1,6 @@
 package testApp;
 
-import java.util.Scanner;
+
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
@@ -11,27 +11,29 @@ public class Splitter {
     int b;
     int sign; // 1(сложение), 2(вычетание), 3(умножение), 4(деление)
     boolean ar; // true - арабские, false - римские
+    String inputNum;
 
-    public Splitter() throws InputException {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        Converter conv = new Converter();
 
+    public  Splitter(String inputNum) throws InputException {
+        this.inputNum = inputNum;
+        Split();
+    }
+    private void Split() throws InputException {
         String arabPattern = "([1-9]|(10))";
         String romePattern = "(I|II|III|IV|V|VI|VII|VIII|IX|X)";
         Pattern pattern1 = Pattern.compile("(" + arabPattern + "[+*/-]"+arabPattern+ ")|(" + romePattern + "[+*/-]" + romePattern + ")");
         Pattern pattern2 = Pattern.compile(arabPattern + "[+*/-]"+arabPattern);
-        Matcher matcher1 = pattern1.matcher(s);
-        Matcher matcher2 = pattern2.matcher(s);
+        Matcher matcher1 = pattern1.matcher(inputNum);
+        Matcher matcher2 = pattern2.matcher(inputNum);
 
         if(!matcher1.matches()){
             throw new InputException("Incorrect input");
         }
 
-        String[] nums = s.split("[+/*-]");
-        if (s.contains("+")){this.sign = 1;}
-        else if (s.contains("-")) {this.sign=2;}
-        else if (s.contains("*")) {this.sign=3;}
+        String[] nums = inputNum.split("[+/*-]");
+        if (inputNum.contains("+")){this.sign = 1;}
+        else if (inputNum.contains("-")) {this.sign=2;}
+        else if (inputNum.contains("*")) {this.sign=3;}
         else {this.sign=4;}
 
 
@@ -46,5 +48,7 @@ public class Splitter {
             this.b = convertToArabian(nums[1]);
         }
     }
- }
+}
+
+
 
